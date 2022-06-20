47e81aa1de05e97a2533c1648c163c6db2569659
freiremarcos <marcos.freire@zarpsystem.com.br>
2022-06-20 10:23:52 -0300

    init

---
+++ Api/Controllers/HealthCheckController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("api/health")]
+    public class HealthCheckController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok("");
+    }
+}
---
+++ BoletoNetCore.Pdf/BoletoImpressao/BoletoBancarioPdf.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BoletoNetCore.Pdf.BoletoImpressao
+{
+    public class BoletoBancarioPdf : BoletoBancario
+    {
+        public byte[] MontaBytesPDF(bool convertLinhaDigitavelToImage = false, string urlImagemLogoBeneficiario = null)
+        {
+#if NETSTANDARD2
+            return (new Wkhtmltopdf.NetCore.HtmlAsPdf().GetPDF(MontaHtmlEmbedded(convertLinhaDigitavelToImage, true, urlImagemLogoBeneficiario)));
+#else
+            throw new NotImplementedException();
+#endif
+
+
+        }
+    }
+}
